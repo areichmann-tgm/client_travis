abbf808c1936732a9cd84ea3d4170896c8703fcc
areichmann-tgm <areichmann@student.tgm.ac.at>
2019-01-26 18:40:25 +0100

    Database now doesn't only work on my machine

--- src/main/python/server/rest.py
+++ src/main/python/server/rest.py
@@ -1,3 +1,5 @@
+import sqlite3
+
 from flask import Flask
 from flask_restful import reqparse, abort, Api, Resource
 from sqlalchemy import create_engine
@@ -9,18 +11,30 @@ api: Api = Api(app)
 CORS(app)
 app.config['CORS_HEADERS'] = 'Content-Type'
 
-
-
-
-
-sql_create_projects_table ="""CREATE TABLE IF NOT EXISTS schueler (
+def create():
+    try:
+        conn.execute("""CREATE TABLE IF NOT EXISTS schueler (
  id integer PRIMARY KEY,
  name VARCHAR NOT NULL,
  email VARCHAR,
  bild VARCHAR
-);"""
+);""")
+        conn.execute("""DELETE FROM schueler;""")
+    except:
+        pass
 
-e = create_engine('sqlite:///MyStudents.db')
+def insert():
+    conn.execute("""INSERT INTO schueler (id, name, email, bild)
+              values(1000, "Adrian", "s1@microgreen.com", "NO_PIC")""")
+
+
+db_path = r'MyStudents.db'
+connt = sqlite3.connect(db_path)
+conn = connt.cursor()
+create()
+insert()
+connt.commit()
+
 
 parser = reqparse.RequestParser()
 parser.add_argument('schueler_id')
@@ -32,28 +46,26 @@ parser.add_argument('pictureX')
 class SchuelerGetALL(Resource):
     def get(self):
 
-        conn = e.connect()
-
         query = conn.execute("SELECT * FROM schueler;")
-        return {'schueler': [dict(zip(tuple (query.keys()) ,i)) for i in query.cursor]},200
+        return {'schueler': [dict(zip(tuple (query.fetchall()) ,i)) for i in query]},200
 
 class Schueler(Resource):
 
     def get(self):
-        conn = e.connect()
+
         schueler_id = parser.parse_args().schueler_id
         query = conn.execute("select * from schueler where ID='%s';"%schueler_id)
-        return {'schueler': [dict(zip(tuple (query.keys()) ,i)) for i in query.cursor]},200
+        return {'schueler': [dict(zip(tuple (query.fetchall()) ,i)) for i in query.conn]},200
 
     def delete(self):
-        conn = e.connect()
+
         schueler_id = parser.parse_args().schueler_id
         query = conn.execute("DELETE from schueler where ID='%s';"%schueler_id)
         return 201
 
 
     def put(self):
-        conn = e.connect()
+
         schueler_id = parser.parse_args().schueler_id
         emailX = parser.parse_args().emailX
         usernameX = parser.parse_args().usernameX
@@ -67,7 +79,7 @@ class Schueler(Resource):
         return 201
 
     def update(self):
-        conn = e.connect()
+
         schueler_id = parser.parse_args().schueler_id
         emailX = parser.parse_args().emailX
         usernameX = parser.parse_args().usernameX
