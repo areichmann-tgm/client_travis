1fc59298539c84cc533ac1acbb9f2df946ce709e
areichmann-tgm <areichmann@student.tgm.ac.at>
2019-01-27 15:14:11 +0100

    Mehr Tests

--- src/main/python/client/frontend/cypress/integration/test.spec.js
+++ src/main/python/client/frontend/cypress/integration/test.spec.js
@@ -5,9 +5,9 @@ describe('My First Test', function() {
 
     it('Quering for Tableheaders', function(){
         cy.visit('http://localhost:8080/')
-        cy.contains('id')
-        cy.contains('username')
-        cy.contains('email')
+        cy.contains('ID')
+        cy.contains('Email')
+        cy.contains('Benutzername')
     })
 
     it('Checking if Buttons clickable', function(){
